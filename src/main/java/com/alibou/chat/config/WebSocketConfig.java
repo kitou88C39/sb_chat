a7ab2c8d84d274a5327a4cad9fbb61315336c5cb
kitou88C39 <kanfutrooper@icloud.com>
2023-07-13 20:49:37 +0900

    config/WebSocketConfig.javaを編集

--- src/main/java/com/alibou/chat/config/WebSocketConfig.java
+++ src/main/java/com/alibou/chat/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws").withSocketJS();
+        registry.addEndpoint("/ws").withSockJS();
     }
 
     @Override
---
+++ src/main/java/com/alibou/chat/config/WebSocketEventLister.java
@@ -0,0 +1,5 @@
+package com.alibou.chat.config;
+
+public class WebSocketEventLister {
+    
+}
