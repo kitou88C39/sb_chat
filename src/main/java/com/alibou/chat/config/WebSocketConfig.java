21df99e40eb2eab5e000a32edfb9d5cf43306fca
kitou88C39 <kanfutrooper@icloud.com>
2023-07-13 20:44:50 +0900

    config/WebSocketConfig.javaを編集

--- src/main/java/com/alibou/chat/config/WebSocketConfig.java
+++ src/main/java/com/alibou/chat/config/WebSocketConfig.java
@@ -1,12 +1,24 @@
 package com.alibou.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/ws").withSocketJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry){
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
 }
